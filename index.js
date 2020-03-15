92358478a4df15c92e4b5319f3416e6b8aeac2db
Giuseppe Tropea <giutropea@gmail.com>
2020-03-15 20:18:56 +0100

    using this for plotting some coronavirus Italy statistics

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ let students = JSON.parse(data)
 //)
 
 
-
-const merge_arrays = (a1, a2) => a1.map( (x, i) => [x, a2[i]] )  // why ramda.map does NOT work here?
+// utility function follows: it uses the second parameter "index" of the .map()'s
+// callback signature. Can ramda.map does work here?
+const merge_arrays = (a1, a2) => a1.map( (x, i) => [x, a2[i]] ) // WARNING: THIS IS NOT IMMUTABLE
 
 
 const extract_scores_of_a_students_results = results => ramda.map(result => result.score, results)
@@ -63,3 +64,96 @@ ramda.pipe(
     ramda.head,
 )(averages)
 )
+
+
+const date_to_unix = datestring => parseInt((new Date(datestring).getTime() / 1000).toFixed(0))
+
+
+
+
+let datacv = fs.readFileSync('covid_stripped.json')
+let corona = JSON.parse(datacv)
+
+const regions_set = new Set(corona.map(region => region.denominazione_regione))
+const all_regions = Array.from(regions_set)
+
+
+console.log(all_regions)
+
+
+// pick up the most hit regions
+const find_coronamax_for_a_region = region =>
+  corona.filter(datapoint => datapoint.denominazione_regione == region).
+  map(datapoint => datapoint.totale_attualmente_positivi).
+  sort((a, b) => b - a)[0] // For descending sort, first element should be the highest
+
+
+// stamp out what the timeshimax affected people figure is for each region
+const maxed_regions = all_regions.map(region => ({regionname:region, maxcorone:find_coronamax_for_a_region(region)}))
+
+console.log(maxed_regions)
+
+let regions = maxed_regions.sort((a, b) => b.maxcorone - a.maxcorone).slice(0, 5).map(region => region.regionname)
+
+console.log(regions)
+
+regions.push('Sicilia')
+
+console.log(regions)
+
+
+
+const find_timeshift_for_a_region = (common_hook_value, region) =>
+  corona.filter(datapoint => datapoint.denominazione_regione == region && datapoint.totale_attualmente_positivi <= common_hook_value).
+  map(datapoint => date_to_unix(datapoint.data)).
+  sort((a, b) => b - a)[0] // For descending sort, first element should be the highest timestamp of all elements having positivi < hook
+//  filter((timestamp, index, array) => index == 0 || index == array.length-1). // keep just the first and the last
+//  reduce((subtractor, current) => subtractor - current)
+
+
+// figure out what the timeshift is for each region
+// It first creates an array of objects, with region_name and its timeshift,
+// then assigns all objects of the array to the first object of the array
+// basically merging them into a hashmap object. All object of the areray are
+// expanded using the sprea dsyntax
+const generate_timeshifts = common_hook_value =>
+  Object.assign(...
+    // here using the string "region" as key to object
+    regions.map(region => ({[region]:find_timeshift_for_a_region(common_hook_value, region)}))
+  )
+
+
+const timeshifts = generate_timeshifts(166) // at least 166 here, Lombardia first value...
+
+
+console.log(timeshifts)
+
+
+const timeshift_and_cleanup_a_date_for_a_region = (date, region) => {
+    return (date_to_unix(date) - timeshifts[region]) / 86400
+//  d = new Date((date_to_unix(date) - timeshifts[region]) * 1000)
+//  return (d.getMonth()+1).toString() + "-" + d.getDate().toString()
+//  return d.toString()
+}
+
+// the last replace uses the regexp /,/g
+// where g is the global flag to replace ALL commas in the resulting big string with void.
+// Also the "data" (timestamp) field is split and just the day and not the hour is used
+// Please notice the newlines to space apart the plots in gnuplots input format
+const output = regions.map(region => `TIME ${region}
+`+corona.filter(datapoint => datapoint.denominazione_regione == region).
+map(datapoint => `${timeshift_and_cleanup_a_date_for_a_region(datapoint.data, region)} ${datapoint.totale_attualmente_positivi}
+`).toString()+`
+
+
+`).toString().replace(/,/g,'').replace(/2020-/g,'')
+
+
+
+//console.log(output)
+
+
+
+fs.writeFile('corona_regions_output.txt', output, (err) => { 
+        if (err) throw err; 
+    })
---
+++ plot_regions.gnuplot
@@ -0,0 +1,23 @@
+# this uses Xlabels from the file
+set title "Coronavirus diffusion in Italy's regions"
+set label 1 "Data from official repository https://github.com/pcm-dpc/COVID-19"  font ",6"
+set label 1 at graph 0.02, 0.85 tc lt 3
+set label 2 "Giuseppe Tropea giutropea\\@gmail.com" font ",6"
+set label 2 at graph 0.02, 0.80 tc lt 3
+set style data lines
+set ylabel "total positive cases"
+#set autoscale
+set autoscale y
+set xrange [-5:*]
+#set yrange [0:2000]
+set key autotitle columnhead
+set key font ",6"
+set terminal pdf
+set output "corona_regions_graph.pdf"
+set xtics font ",6"
+plot 'corona_regions_output.txt' using 1:2 index 0, \
+     ''                          using 1:2 index 1, \
+     ''                          using 1:2 index 2, \
+     ''                          using 1:2 index 3, \
+     ''                          using 1:2 index 4, \
+     ''                          using 1:2 index 5
